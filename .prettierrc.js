6648cad4bd6481f1cc8a01d9d29f0b4f839a5286
JawadAhbab <jawad.ahbab.turna@gmail.com>
2023-09-25 15:06:04 +0600

    🎉 Version 1.0.0

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: ['./src/index.js'],
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const tsparser = require('prettier/parser-typescript')
+const estree = require('prettier/plugins/estree')
+
+module.exports = {
+  parsers: { typescript: tsparser.parsers.typescript },
+  printers: {
+    estree: {
+      ...estree.printers.estree,
+      print: (path, options, print) => {
+        const value = estree.printers.estree.print(path, options, print)
+        if (value[0] !== 'import') return value 
+
+        const line = []
+        value.flat(Infinity).forEach(vi => {
+          if (typeof vi === 'string') return line.push(vi)
+          if (vi.type !== 'group') return line.push(vi)
+          vi.contents.flat(Infinity).forEach(ci => {
+            if (typeof ci === 'string') return line.push(ci)
+            if (ci.type !== 'indent') return
+            ci.contents.flat(Infinity).forEach(cci => {
+              if (typeof cci === 'string') return line.push(cci)
+              if (cci.type === 'line') return line.push(' ')
+            })
+            line.push(' ')
+          })
+        })
+        return line
+      },
+    },
+  },
+}
